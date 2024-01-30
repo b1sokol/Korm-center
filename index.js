44ad57cd4836fef6f06558336c3a1758c0da0022
b1sokol <113973047+b1sokol@users.noreply.github.com>
2024-01-30 21:09:22 +0100

    30.01

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const categoryList = document.querySelector('.category-list');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const categoryList = document.querySelector('.category-list');
   
-    categoryList.addEventListener('mouseover', function (event) {
-      if (event.target.tagName === 'A') {
-        event.target.style.color = '#d32f2f';
+//     categoryList.addEventListener('mouseover', 
+//     function (event) {
+//       if (event.target.tagName === 'A') {
+//         event.target.style.color = '#d32f2f';
   
-        const subMenu = event.target.nextElementSibling;
-        if (subMenu) {
-          subMenu.style.display = 'block';
-        }
-      }
-    });
+//         const subMenu = event.target.nextElementSibling;
+//         if (subMenu) {
+//           subMenu.style.display = 'block';
+//         }
+//       }
+//     });
   
-    categoryList.addEventListener('mouseout', function (event) {
-      if (event.target.tagName === 'A') {
-        event.target.style.color = '#333';
+//     categoryList.addEventListener('mouseout', function (event) {
+//       if (event.target.tagName === 'A') {
+//         event.target.style.color = '#333';
   
-        const subMenu = event.target.nextElementSibling;
-        if (subMenu) {
-          subMenu.style.display = 'none';
-        }
-      }
-    });
+//         const subMenu = event.target.nextElementSibling;
+//         if (subMenu) {
+//           subMenu.style.display = 'none';
+//         }
+//       }
+//     });
   
-    // Додано новий обробник для випадаючих списків
-    document.addEventListener('mouseover', function (event) {
-      const subMenu = event.target.closest('.sub-menu, .nested-sub-menu');
-      if (!subMenu) {
-        // Якщо клік не на елементі випадаючого списку, ховаємо його
-        const allSubMenus = document.querySelectorAll('.sub-menu, .nested-sub-menu');
-        allSubMenus.forEach((menu) => {
-          menu.style.display = 'none';
-        });
-      }
-    });
-  });
+
+//     document.addEventListener('mouseover', function (event) {
+//       const subMenu = event.target.closest('.nested-sub-menu');
+//       if (!subMenu) {
+
+//         const allSubMenus = document.querySelectorAll('.nested-sub-menu');
+//         allSubMenus.forEach((menu) => {
+//           menu.style.display = 'none';
+//         });
+//       }
+//     });
+//   });
   
